44ca92e23dfdbbc34e346f7f5865704e441b8023
Anikait Makkar <anikait.makkar@outlook.com>
2024-03-18 23:43:02 +0530

    finalize fastapi server

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
-import datetime
 import argparse
 import os
 import pprint
-from dataclasses import dataclass
 
-from py_pdf_parser.loaders import load_file, PDFDocument
+from py_pdf_parser.loaders import load_file
 
-
-@dataclass
-class Station:
-    name: str
-    time: datetime.datetime
-
-
-@dataclass
-class Train:
-    number: int
-    name: str
-
-
-@dataclass
-class Ticket:
-    departure: Station
-    arrival: Station
-    pnr: int
-    price: float
-    train: Train
+import ticket_parser as TicketParser
 
 
 class CustomArgsNamespace:
@@ -34,70 +13,6 @@ class CustomArgsNamespace:
 
     def __repr__(self) -> str:
         return f"Dir({self.dir}) Files({self.files})"
-
-
-def _parse_station(document: PDFDocument, station_key: str, timing_key: str):
-    station_element = document.elements.filter_by_text_equal(
-        station_key
-    ).extract_single_element()
-    station_name = document.elements.below(station_element)[0].text()
-
-    regex_pattern = rf"{timing_key}\*\s\d{{2}}:\d{{2}}\s\d{{1,2}}\s\w{{3}}\s\d{{4}}"
-    _, time = (
-        document.elements.filter_by_regex(regex_pattern).extract_single_element().text()
-    ).split(f"{timing_key}*")
-
-    return Station(
-        name=station_name,
-        time=datetime.datetime.strptime(time.strip(), "%H:%M %d %b %Y"),
-    )
-
-
-def _parse_pnr(document: PDFDocument) -> int:
-    pnr_text_element = document.elements.filter_by_text_equal(
-        "PNR"
-    ).extract_single_element()
-    pnr = document.elements.below(pnr_text_element)[0].text()
-
-    return int(pnr)
-
-
-def _parse_final_price(document: PDFDocument) -> float:
-    total_fare_text_element = document.elements.filter_by_text_equal(
-        "Total Fare (all inclusive)"
-    ).extract_single_element()
-    price_text_label = (
-        document.elements.to_the_right_of(total_fare_text_element)
-        .extract_single_element()
-        .text()
-    )
-
-    price = (
-        price_text_label.split("₹")[-1] if "₹" in price_text_label else price_text_label
-    )
-    return float(price.strip())
-
-
-def _parse_train(document: PDFDocument) -> Train:
-    train_info_label_element = document.elements.filter_by_text_equal(
-        "Train No./Name"
-    ).extract_single_element()
-    train_info = document.elements.below(train_info_label_element)[0].text()
-
-    number, name = train_info.split("/")
-    return Train(int(number.strip()), name.strip())
-
-
-def parse_ticket(document: PDFDocument) -> Ticket:
-    departure = _parse_station(document, "Booked From:", "Departure")
-    arrival = _parse_station(document, "To:", "Arrival")
-    pnr = _parse_pnr(document)
-    price = _parse_final_price(document)
-    train = _parse_train(document)
-
-    return Ticket(
-        departure=departure, arrival=arrival, pnr=pnr, price=price, train=train
-    )
 
 
 def setup_command_line_args() -> CustomArgsNamespace:
@@ -129,7 +44,7 @@ def main():
     args = setup_command_line_args()
     files = args.files if len(args.files) > 0 else read_filenames_directory(args.dir)
 
-    tickets = [parse_ticket(load_file(filename)) for filename in files]
+    tickets = [TicketParser.parse_ticket(load_file(filename)) for filename in files]
     pprint.pprint(tickets)
 
 
--- server.py
+++ server.py
@@ -1,49 +1,60 @@
-from datetime import datetime
-import gc
 from io import BytesIO
-import os
-from pprint import pprint
-import random
-from tempfile import NamedTemporaryFile
-from typing import BinaryIO
 
-from fastapi import FastAPI, HTTPException, Request, File, UploadFile
+from fastapi import FastAPI, HTTPException, Header, Request, UploadFile
+from fastapi.params import Depends
 from starlette import status
 from fastapi.templating import Jinja2Templates
-from py_pdf_parser.loaders import load_file, PDFDocument, load
-
+from py_pdf_parser.loaders import load
+import resend
 
 import ticket_parser as TicketParser
 
 app = FastAPI()
-# templates = Jinja2Templates(directory="templates")
-MAX_ALLOWED_FILE_LIMIT = 5_242_880 # 5MB
+templates = Jinja2Templates(directory="templates")
+MAX_ALLOWED_FILE_LIMIT = 5_242_880  # 5MB
 
 
-# @app.get("/")
-# async def index(request: Request):
-#     return templates.TemplateResponse(request=request, name="index.html")
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
+
+
+async def valid_content_length(
+    content_length: int = Header(..., lt=MAX_ALLOWED_FILE_LIMIT)
+):
+    return content_length
+
 
 @app.post("/send-email")
-async def send_ticket_email(request: Request, file: UploadFile):
-    # file.file.seek(0, os.SEEK_END)
-    file_size = 0
-
-    a = BytesIO()
+async def send_ticket_email(
+    request: Request, file: UploadFile, file_size=Depends(valid_content_length)
+):
+    inmemory_file = BytesIO()
     for chunk in file.file:
-        file_size += len(chunk)
-        if file_size > MAX_ALLOWED_FILE_LIMIT:
+        inmemory_file.write(chunk)
+        if inmemory_file.tell() > MAX_ALLOWED_FILE_LIMIT:
             raise HTTPException(status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE)
 
-        a.write(chunk)
-    
-    a.seek(0)
-    
-    now = datetime.now()
-    document = load(a)
+    inmemory_file.seek(0)
+    print("starting file parse")
+    try:
+        document = load(inmemory_file)
+    except Exception as error:
+        print("Error loading file")
+        # TODO - generic api error handler
+        pass
+
     ticket = TicketParser.parse_ticket(document)
-    end = datetime.now()
-    pprint(ticket)
 
+    resend.api_key = "<API>"
 
-    return f'{(end - now).microseconds / 1000}'
+    r = resend.Emails.send({
+    "from": "onboarding@resend.dev",
+    "to": "anikait.makkar@outlook.com",
+    "subject": "Hello World",
+    "html": "<p>Congrats on sending your <strong>first email</strong>!</p>"
+    })
+
+    print(r)
+
+    return "success"
